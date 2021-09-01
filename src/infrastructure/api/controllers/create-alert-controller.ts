f49fdcf11d7b26cd4a398034f3bddd253d0caaae
Felix Brockmeier <felix.brockmeier@outlook.com>
2021-09-01 11:12:24 +0200

    create alert fix

--- src/infrastructure/api/controllers/create-alert-controller.ts
+++ src/infrastructure/api/controllers/create-alert-controller.ts
@@ -1,6 +1,10 @@
 // TODO: Violation of control flow. DI for express instead
 import { Request, Response } from 'express';
-import { CreateAlert, CreateAlertRequestDto, CreateAlertResponseDto } from '../../../domain/alert/create-alert';
+import {
+  CreateAlert,
+  CreateAlertRequestDto,
+  CreateAlertResponseDto,
+} from '../../../domain/alert/create-alert';
 import Result from '../../../domain/value-types/transient-types/result';
 import { BaseController, CodeHttp } from '../../shared/base-controller';
 
@@ -12,33 +16,44 @@ export default class CreateAlertController extends BaseController {
     this.#createAlert = createAlert;
   }
 
-  #buildRequestDto = (
-    httpRequest: Request
-  ): Result<CreateAlertRequestDto> => {
-    const { selectorId } = httpRequest.query;
-    if (typeof selectorId === 'string')
-      return Result.ok<CreateAlertRequestDto>({
-        selectorId,
-      });
-    return Result.fail<CreateAlertRequestDto>(
-      'request query parameter automationId is supposed to be in string format'
-    );
+  #buildRequestDto = (httpRequest: Request): Result<CreateAlertRequestDto> => {
+    const { selectorId } = httpRequest.params;
+
+    if (!selectorId)
+      return Result.fail<CreateAlertRequestDto>(
+        'Cannot find request parameter selectorId'
+      );
+
+    return Result.ok<CreateAlertRequestDto>({
+      selectorId,
+    });
   };
 
   protected async executeImpl(req: Request, res: Response): Promise<Response> {
     try {
-      const buildDtoResult: Result<CreateAlertRequestDto> = this.#buildRequestDto(req);
+      const buildDtoResult: Result<CreateAlertRequestDto> =
+        this.#buildRequestDto(req);
 
-      if(buildDtoResult.error) return CreateAlertController.badRequest(res, buildDtoResult.error);
-      if(!buildDtoResult.value) return CreateAlertController.badRequest(res, 'Invalid request query paramerters');
+      if (buildDtoResult.error)
+        return CreateAlertController.badRequest(res, buildDtoResult.error);
+      if (!buildDtoResult.value)
+        return CreateAlertController.badRequest(
+          res,
+          'Invalid request paramerters'
+        );
 
-      const useCaseResult : CreateAlertResponseDto = await this.#createAlert.execute(buildDtoResult.value);
+      const useCaseResult: CreateAlertResponseDto =
+        await this.#createAlert.execute(buildDtoResult.value);
 
       if (useCaseResult.error) {
         return CreateAlertController.badRequest(res, useCaseResult.error);
       }
 
-      return CreateAlertController.ok(res, useCaseResult.value, CodeHttp.CREATED);
+      return CreateAlertController.ok(
+        res,
+        useCaseResult.value,
+        CodeHttp.CREATED
+      );
     } catch (error) {
       return CreateAlertController.fail(res, error);
     }
